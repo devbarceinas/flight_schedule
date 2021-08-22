efcf0932b61c8c0a4aa52da7ed42b69f7e5266bb
devbarceinas <dev.mariobarceinas@gmail.com>
2021-08-22 02:38:54 -0500

    feature: Se creo el componente del forumlario y se agrego el mock

---
+++ src/db/mock.js
@@ -0,0 +1,13 @@
+{
+  origin: [
+    {
+      name: "Ciudad de México, México",
+    },
+    {
+      name: "Ciudad de Sapaulo, Brasil"
+    },
+    {
+      name: "Ciudad de Tokyo, Japon"
+    }
+  ]
+}
